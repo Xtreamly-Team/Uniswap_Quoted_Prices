a6dd205267d1c80daa771c1b55ea57772ab62693
Eren Nevin <eren.m.nevin@gmail.com>
2024-02-24 23:33:19 +0300

    Changed provider to AWS

--- src/providers.ts
+++ src/providers.ts
@@ -1,5 +1,5 @@
 import { ethers, providers } from 'ethers'
 
 export function getProvider(): providers.Provider {
-  return new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/989a33ef7f114f45b58346df500b9917')
+  return new ethers.providers.JsonRpcProvider('nd-hrmrouztdfhwnmw5pn255q3i7a.ethereum.managedblockchain.eu-west-2.amazonaws.com')
 }
